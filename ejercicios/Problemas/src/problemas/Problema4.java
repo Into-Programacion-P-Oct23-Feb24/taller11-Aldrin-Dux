04ff866a915a5b379f1bc36487d63cf72491e8c3
Aldrin Padilla Cairo <apadilla2@utpl.edu.ec>
2024-01-05 21:57:25 -0600

    primer intento

--- ejercicios/Problemas/src/problemas/Problema4.java
+++ ejercicios/Problemas/src/problemas/Problema4.java
@@ -5,6 +5,9 @@
  */
 package problemas;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +19,58 @@ public class Problema4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int tipo;
+        double planilla;
+        double planilla2;
+        System.out.println("Ingrese el tipo de area a sacar\n(1)Para calcular el"
+                + "valor de la planilla de luz\n(2)Para calcular el valor por el"
+                + " inmueble");
+        tipo = entrada.nextInt();
+        if (tipo == 1) {
+            entrada.nextLine();
+            System.out.println("Ingresar nombre");
+            String nombre = entrada.nextLine();
+
+            System.out.println("Ingresar cedula");
+            int cedula = entrada.nextInt();
+            System.out.println("Ingresar valor del kilovatio");
+            double valor = entrada.nextDouble();
+            System.out.println("Ingresar cantidad usada");
+            double cantidad = entrada.nextDouble();
+            planilla = calcularValorLuz(valor, cantidad);
+            System.out.printf("Cliente %s con cédula %d debe canelar el "
+                    + "valor de: $%.1f\n", nombre, cedula, planilla);
+        } else {
+            if (tipo == 2) {
+                entrada.nextLine();
+                System.out.println("Ingresar nombre");
+                String nombre2 = entrada.nextLine();
+
+                System.out.println("Ingresar cedula");
+                int cedula = entrada.nextInt();
+                System.out.println("Ingresar valor del inmueble");
+                double valor = entrada.nextDouble();
+                planilla2 = calcularPrecio(valor);
+                System.out.printf("Cliente %s con cédula %d tiene un bien inmueble"
+                        + "valorado en $%.0f y tiene que pagar un precio de $%.0f\n",
+                        nombre2, cedula, valor, planilla2);
+            } else {
+                System.out.println("tipo ingresado es incorrecto");
+            }
+        }
     }
-    
+
+    public static double calcularValorLuz(double v, double c) {
+        double plan;
+        plan = v * c;
+        return plan;
+    }
+
+    public static double calcularPrecio(double v) {
+        double inmueble;
+        inmueble = (v * 0.02);
+        return inmueble;
+    }
 }
--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -10,5 +10,21 @@ package problemas;
  * @author reroes
  */
 public class Problema5 {
-    
+
+    public static void main(String[] args) {
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        imprimirSuma(informacion);
+    }
+
+    public static void imprimirSuma(int[][] suma) {
+        int suma1 = 0;
+        int suma2;
+        for (int f = 0; f < suma.length; f++) {
+            for (int c = 0; c < suma[f].length; c++) {
+                suma2 = suma[f][c];
+                suma1 = suma1 + suma2;
+            }
+        }
+        System.out.printf("Suma total del arreglo es: %d \n", suma1);
+    }
 }
